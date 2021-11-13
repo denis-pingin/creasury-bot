741a9f2e298b0ca19757a86a920fafd39a2ae3ea
Denis Pingin <2085934+denis-pingin@users.noreply.github.com>
2021-11-13 23:05:42 +0100

    add stages config

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -12,12 +12,12 @@ export default async function handleGuildMemberAdd(client, member, inviter) {
     await updateGlobalCounterAndLog(client, 'rejoins', addMemberResult.member.user, member.guild.id, 1);
   }
 
-  let message = `${getUserTag(member.user)} has ${addMemberResult.rejoin ? 're-' : ''}joined the Creasury community.\nThey were originally invited by `;
+  let message = `${getUserTag(member.user)} has ${addMemberResult.rejoin ? 're-' : ''}joined the Creasury community.\nThey were ${addMemberResult.rejoin ? 'originally ' : ''}invited by `;
   if (addMemberResult.member.originalInviter) {
     if (fake) {
       await updateGlobalCounterAndLog(client, 'fakes', addMemberResult.member.originalInviter, member.guild.id, 1);
-      message += `${getUserTag(addMemberResult.member.originalInviter)}, who unfortunately won't get any points because the minimum account age requirements weren't met` +
-        ` (invited accounts must be older than ${config.minAccountAge} days).`;
+      message += `${getUserTag(addMemberResult.member.originalInviter)}, who unfortunately won't get any points because the minimum account age requirements weren't met ` +
+        `(invited accounts must be older than ${config.minAccountAge} days).`;
     } else {
       const inviteCount = await updateGlobalCounterAndLog(client, 'invites', addMemberResult.member.originalInviter, member.guild.id, 1);
       message += `${getUserTag(addMemberResult.member.originalInviter)}, who just gained 1 point and now has ${inviteCount} ${inviteCount === 1 ? 'point' : 'points'} in total.`;
